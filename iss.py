0cbcbf9e86e1ca69d0e20cd25481b89d74a93e1c
Neal Magee <nem2p@virginia.edu>
2025-04-15 09:36:20 -0400

    More

--- iss.py
+++ iss.py
@@ -4,25 +4,34 @@ import datetime
 
 url = "http://api.open-notify.org/iss-now.json"
 
-response = requests.get(url)
+try:
+    response = requests.get(url)
+    r = response.json()
 
-r = response.json()
+    # fetch values from response
+    timestamp = r['timestamp']
+    # convert timestamp to human readable date and time (out of epoch)
+    datetime = datetime.datetime.fromtimestamp(timestamp)
+    dtime = datetime.strftime('%Y-%m-%d-%H:%M:%S')
 
-timestamp = r['timestamp']
-# convert timestamp to human readable date and time (out of epoch)
-datetime = datetime.datetime.fromtimestamp(timestamp)
-dtime = datetime.strftime('%Y-%m-%d-%H:%M:%S')
+    # get longitude and latitude
+    long = r['iss_position']['longitude']
+    lat = r['iss_position']['latitude']
 
-long = r['iss_position']['longitude']
-lat = r['iss_position']['latitude']
+    # print output to console
+    print(dtime)
+    print(long)
+    print(lat)
 
-print(dtime)
-print(long)
-print(lat)
+except Exception as e:
+    print(e)
+    exit(1)
 
-lines = [dtime, "\n",long, "\n", lat]
-
-with open('/data/output.txt', 'w') as f:
-    f.writelines(lines)
-
-# container needs a place to write and store output
+# write output to file. Could also be to external database or other system.
+try:
+    lines = [dtime, "\n", long, "\n", lat]
+    with open('/data/output.txt', 'w') as f:
+        f.writelines(lines)
+except Exception as e:
+    print(e)
+    exit(1)
