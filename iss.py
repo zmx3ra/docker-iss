eb8c1645f5d685b7ceafd0e507cc6e469b1272ed
N Magee <nem2p@virginia.edu>
2025-04-16 09:51:26 -0400

    Tracking with extensions

--- iss.py
+++ iss.py
@@ -1,37 +1,61 @@
 import requests
 import json
 import datetime
+import logging
+import os
+from pymongo import MongoClient
 
-url = "http://api.open-notify.org/iss-now.json"
+# logging config
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
-try:
-    response = requests.get(url)
-    r = response.json()
+def get_iss_location():
+    url = "http://api.open-notify.org/iss-now.json"
+    try:
+        response = requests.get(url)
+        r = response.json()
 
-    # fetch values from response
-    timestamp = r['timestamp']
-    # convert timestamp to human readable date and time (out of epoch)
-    datetime = datetime.datetime.fromtimestamp(timestamp)
-    dtime = datetime.strftime('%Y-%m-%d-%H:%M:%S')
+        # fetch values from response
+        timestamp = r['timestamp']
+        # convert timestamp to human readable date and time (out of epoch)
+        dt_obj = datetime.datetime.fromtimestamp(timestamp)
+        dtime = dt_obj.strftime('%Y-%m-%d-%H:%M:%S')
 
-    # get longitude and latitude
-    long = r['iss_position']['longitude']
-    lat = r['iss_position']['latitude']
+        # get longitude and latitude
+        long = r['iss_position']['longitude']
+        lat = r['iss_position']['latitude']
 
-    # print output to console
-    print(dtime)
-    print(long)
-    print(lat)
+        # log output for visibility
+        logger.info("Timestamp: " + dtime)
+        logger.info("Longitude: " + long)
+        logger.info("Latitude: " + lat)
 
-except Exception as e:
-    print(e)
-    exit(1)
+        # write output to mongo db
+        write_to_mongo(dtime, long, lat)
 
-# write output to file. Could also be to external database or other system.
-try:
-    lines = [dtime, "\n", long, "\n", lat]
-    with open('/data/output.txt', 'w') as f:
-        f.writelines(lines)
-except Exception as e:
-    print(e)
-    exit(1)
+    except Exception as e:
+        logger.error(e)
+        exit(1)
+
+def write_to_mongo(dtime, long, lat):
+    # write output to mongo db
+    try:
+        # use an ENV variable for the password
+        dbpass = os.getenv('MONGOPASS')
+        if not dbpass:
+            raise ValueError("MONGOPASS environment variable is not set")
+            logging.error("MONGOPASS environment variable is not set")
+            exit(1)
+            
+        connection_string = f'mongodb+srv://docker:{dbpass}@cluster0.m3fek.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+        client = MongoClient(connection_string)
+        db = client['iss_location']
+        collection = db['locations']
+        collection.insert_one({'timestamp': dtime, 'longitude': long, 'latitude': lat})
+        logger.info('Output written to MongoDB')
+    except Exception as e:
+        logger.error(e)
+        exit(1)
+
+if __name__ == "__main__":
+    get_iss_location()
