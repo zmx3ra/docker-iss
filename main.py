71b07432d3a1db631eb1697fb747ec2edcb8089d
Neal Magee <nem2p@virginia.edu>
2025-04-16 14:49:08 -0400

    Updated DB name and comment in .py

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ import logging
 import os
 from pymongo import MongoClient
 
+"""
+    This script is used to get the current location of the ISS from the Open Notify API 
+      and write it to a MongoDB database.
+    The script is containerized and can be run anywhere.
+    Note that (a) an entrypoint function is defined in this script, and that (b) error handling
+      and (c) logging are implemented.
+
+    YOU MUST update two things before running this script:
+    1. Populate a MONGOPASS environment variable with your MongoDB password (see Canvas).
+    2. Update the db name to your UVA computing ID on line 68.
+"""
+
 # logging config
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+# core fcn
 def get_iss_location():
     url = "http://api.open-notify.org/iss-now.json"
     try:
@@ -37,6 +50,7 @@ def get_iss_location():
         logger.error(e)
         exit(1)
 
+# db utility fcn
 def write_to_mongo(dtime, long, lat):
     # write output to mongo db
     try:
@@ -59,5 +73,6 @@ def write_to_mongo(dtime, long, lat):
         logger.error(e)
         exit(1)
 
+# entrypoint fcn
 if __name__ == "__main__":
     get_iss_location()
